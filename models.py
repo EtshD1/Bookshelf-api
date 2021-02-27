887516b90608ebbc590fcdacd1811b9965be79e0
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-27 19:52:53 +0200

    add setup method for testing

--- models.py
+++ models.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+def setup_db(app, database_path='postgresql://etsh:3894@127.0.0.1:5432/libraryapi'):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
 class Book(db.Model):
     __tablename__ = 'books'
 
