f957c84ae672c0b71c9018f7353a021b59c6654c
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-27 13:56:36 +0200

    create app inside create_app function

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from flask_cors.decorator import cross_origin
 from models import db, Book
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://etsh:3894@127.0.0.1:5432/libraryapi'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-db.init_app(app)
-migrate = Migrate(app, db)
+__BOOKS_PER_SHELF__ = 5
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://etsh:3894@127.0.0.1:5432/libraryapi'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
     cors = CORS(app, resources={r'/api/*': {"origins": "*"}})
 
@@ -31,17 +31,24 @@ def create_app(test_config=None):
     @app.route('/books')
     def get_books():
         page = request.args.get('page', 1, type=int)
-        start = (page-1)*10
+        start = (page-1) * __BOOKS_PER_SHELF__
         books = Book.query.all()
 
-        limit = start+10
-        if start+10 > len(books):
+        limit = start + __BOOKS_PER_SHELF__
+        if limit > len(books):
             limit = len(books)
 
-        bookList = [book.to_dict() for book in books]
+        bookList = []
+        for book in range(start, limit):
+            bookList.append(books[book].to_dict())
+
+        if len(bookList) == 0:
+            abort(404)
+
         return jsonify({
             'success': True,
-            'books': bookList
+            'books': bookList,
+            'total_books': len(books)
         })
 
     return app
